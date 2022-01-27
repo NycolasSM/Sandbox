10bcc2074045382df345ea3fd79d772ba9a5aaf0
Nycolas Santana <nycolassantana00@gmail.com>
2022-01-27 18:22:30 -0300

    update

--- components/portfolio/src/components/EditableTitle/EditableTitle.tsx
+++ components/portfolio/src/components/EditableTitle/EditableTitle.tsx
@@ -9,7 +9,7 @@ type Props = {
 const EditableTitle: React.FC<Props> = ({ nome = "Titulo Editavel" }) => {
 
   const [fontWeight, setFontWeight] = useState("normal")
-  const [fontSize, setFontSize] = useState("60px")
+  const [fontSize, setFontSize] = useState(60)
   const [fontStyle, setFontStyle] = useState("normal")
   const [textDecoration, setTextDecoration] = useState("")
 
@@ -29,27 +29,46 @@ const EditableTitle: React.FC<Props> = ({ nome = "Titulo Editavel" }) => {
     setFontSize(e.target.value)
   }
 
+  const fontSizeResize = (operator: string) => {
+    if (fontSize <= 60) {
+      if (operator === "+") {
+        setFontSize(fontSize + 1)
+        console.log(fontSize)
+      } 
+    }
+    if (fontSize > 30) {
+      if (operator === "-") {
+        setFontSize(fontSize - 1)
+      }
+    }
+  }
+
   return (
     <div className="editable-title">
       <input type="text" name="profileName" placeholder="Seu Nome" spellCheck={false} 
         style={{ 
           fontWeight: `${fontWeight}`,
-          fontSize: `${fontSize}`,
+          fontSize: `${fontSize}px`,
           fontStyle: `${fontStyle}`,
           textDecoration: `${textDecoration}`,
         }} />
       <div className="edit-tools-profile-name">
-        <button onClick={() => toggleFontBold()} >B</button>
-        <button onClick={() => toggleFontItalic()} >i</button>
-        <button onClick={() => toggleTextUnderline()} >_</button>
+        <button style={{fontWeight: "bold"}} onClick={() => toggleFontBold()} >B</button>
+        <button style={{fontStyle: "italic", fontFamily: "serif", fontWeight: "bold"}} onClick={() => toggleFontItalic()} >I</button>
+        <button onClick={() => toggleTextUnderline()} >U</button>
         <button onClick={() => toggleFontBold()} >B</button>
         <label htmlFor="font-size">Font Size</label>
-        <select onChange={(e) => changeFontSize(e)} id="font-size" name="font-size">
-          <option value="60px">60</option>
-          <option value="50px">50</option>
-          <option value="40px">40</option>
-          <option value="30px">30</option>
+        <select onChange={(e) => changeFontSize(e)} value={fontSize} id="font-size" name="font-size">
+          <option value="30">30</option>
+          <option value="35">35</option>
+          <option value="40">40</option>
+          <option value="45">45</option>
+          <option value="50">50</option>
+          <option value="55">55</option>
+          <option value="60">60</option>
         </select>
+        <button onClick={() => fontSizeResize("+")}>+</button>
+        <button onClick={() => fontSizeResize("-")}>-</button>
       </div>
     </div>
   );
